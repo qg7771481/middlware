96b8dc88c7c7c47771f035f1ca9a58ba2adfbf9c
qg7771481 <ddavidok845@gmail.com>
2025-07-06 19:02:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import JSONResponse
+from datetime import datetime
+from starlette.middleware.base import BaseHTTPMiddleware
+
+app = FastAPI(title="Middleware Example")
+
+
+class CustomMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        method = request.method
+        url = str(request.url)
+        print(f"[{current_time}] {method} запит на {url}")
+
+        if "X-Custom-Header" not in request.headers:
+            return JSONResponse(
+                status_code=400,
+                content={"detail": "Відсутній заголовок X-Custom-Header"}
+            )
+
+        response = await call_next(request)
+        return response
+
+app.add_middleware(CustomMiddleware)
+
+
+@app.get("/hello", tags=["Тести"])
+def say_hello():
+    return {"message": "наверно"}
+
+
+@app.get("/ping", tags=["Тести"])
+def ping():
+    return {"message": "322"}
+
+
+@app.get("/secure", tags=["Тести"])
+def secure_route():
+    return {"message": "Безкоштовний анти вірус"}
